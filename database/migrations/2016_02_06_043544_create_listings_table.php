342c051de0bc6d58eb60097ccd35a8eb947397ba
AlexLawrence95 <al12nj@brocku.ca>
2016-02-06 19:00:32 -0500

    Create 2016_02_06_043544_create_listings_table.php
    -Added the initial version of the create listings table file.

---
+++ database/migrations/2016_02_06_043544_create_listings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Listings extends Migration
+{
+    /**
+     * Run the migrations.
+     * Creates the listings table.
+     * Contains references to the user who created the listing, and the location of that listing.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            //Primary key
+            $table->increments('listing_id');
+
+            //Foreign Key
+            //Referenced by location table, the address of the listing
+            $table->integer('location_id')->unsigned();
+            $table->foreign('location_id')->references('location_id')->on('location');
+
+            //Foreign Key
+            //References the user table, id of the user who put up the listing
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('user_id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * Drops the listings table
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('listings');
+    }
+}
