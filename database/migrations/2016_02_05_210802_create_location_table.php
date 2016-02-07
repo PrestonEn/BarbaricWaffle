5331fe9fc1c8753520d28d77752d9bb04a175f77
AlexLawrence95 <al12nj@brocku.ca>
2016-02-06 19:44:22 -0500

    Update CreateLocationTable File
    -Minor Classname fix

--- database/migrations/2016_02_05_210802_create_location_table.php
+++ database/migrations/2016_02_05_210802_create_location_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Location extends Migration
+class CreateLocationTable extends Migration
 {
     /**
      * Run the migrations.
