0aecf02b86696526f86885b1a2153c375e60b77e
Jaclyn Binch <jb11jr@brocku.ca>
2016-02-08 21:29:40 -0500

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">THIS IS A TEST</div>
+                <div class="title">THIS IS A TEST sskdkdkkd</div>
             </div>
         </div>
     </body>
