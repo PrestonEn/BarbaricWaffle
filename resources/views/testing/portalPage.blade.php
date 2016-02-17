3f8dda1bfc9cad135bf8d585fe96d1dd284ea0cb
Jaclyn Binch <jb11jr@brocku.ca>
2016-02-17 16:05:59 -0500

    Fixed the portal page link issue re houseTemplate

--- resources/views/testing/portalPage.blade.php
+++ resources/views/testing/portalPage.blade.php
@@ -17,7 +17,7 @@
 	<li><a href="profile">profile overview page</a></li>
 	<li><a href="signIn">sign in page</a></li>
 	<li><a href="signUp">sign up page</a></li>
-	<li><a href="houseTemplate"></a>house listing template</li>	
+	<li><a href="houseTemplate">house listing template</a></li>	
 	<li><a href="addListing">Post listing template</a></li>
 	<li><a href="profileFavourites">Favourites</a></li>
 	<li><a href="signUp">sign up page</a></li>
