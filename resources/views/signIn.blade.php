dc679f0ea889c6aca0016a4b06a87dcb21c54a0a
Preston Engstrom <p.engstrom94@gmail.com>
2016-04-04 15:47:21 -0400

    basic authentication working

--- resources/views/signIn.blade.php
+++ resources/views/signIn.blade.php
@@ -7,15 +7,20 @@
 <link href="{!! asset('CSS/signIn.css') !!}" media="all" rel="stylesheet" type="text/css" />
 
 <div class = "pageTitle"> Sign In </div>	
+@if (session('failure'))
+    <div class="alert alert-warning">
+        {{ session('failure') }}
+    </div>
+@endif
 
-
-<form action = "" method = "post">
+<form action = "signIn" method = "post">
+	<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 	<div class = "row">
 		<div class = "col-xs-1 col-sm-2 col-md-3"></div>
 		<div class = "col-xs-10 col-sm-8 col-md-6">
-			Username :  
-			<input type = "text" class = "form-control" name = "pass">
+			Email :  
+			<input type = "text" class = "form-control" name = "email">
 		</div>
 	</div>	
 
@@ -23,7 +28,7 @@
 		<div class = "col-xs-1 col-sm-2 col-md-3"></div>
 		<div class = "col-xs-10 col-sm-8 col-md-6">
 			Password :  
-			<input type = "password" class = "form-control" name = "pass">
+			<input type = "password" class = "form-control" name = "password">
 		</div>
 	</div>	
 
