36d4dd48efe0decb759bf57f9a63557afc0e4217
Jaclyn Binch <jb11jr@brocku.ca>
2016-04-06 14:17:16 -0400

    fixed button issue on Profile Searches page

--- resources/views/navbarLeft.blade.php
+++ resources/views/navbarLeft.blade.php
@@ -9,7 +9,7 @@
 	  			<a href="../profile/12" class="list-group-item active">Profile</a>
 	  			<a href="../profileSearches" class="list-group-item">Saved Searches</a>
 	  			<a href="../profileFavourites/12" class="list-group-item">Favourites</a>
-	  			<a href="../profileMessages" class="list-group-item">Messages</a>
+	  			<!--<a href="../profileMessages" class="list-group-item">Messages</a>-->
 	  			<a href="../profilePostings/12" class="list-group-item">Posted Listings</a>
 	  			<a href="../profileSettings/12" class="list-group-item">Profile Settings</a>
 
