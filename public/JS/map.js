4bbf6b845170dc1284db703bedc73781377a33d9
Jaclyn Binch <jb11jr@brocku.ca>
2016-04-06 22:45:25 -0400

    fixed the routing from marker click events to relevant houseTemplate Started Marker mouseover/mouseout events - will likely require infowindow

--- public/JS/map.js
+++ public/JS/map.js
@@ -2,61 +2,64 @@
 var map;
 var geocoder;
 
-function initMap(arr, ids) {
-  var coords;  
+function initMap(lat,lon,ids) {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position){
-      success(position,arr,ids);
+      success(position,lat,lon,ids);
     });
-    
-
   } else {
     error('Geo Location is not supported');
-    coords = {lat: 43.1, lng: -79.3};
-    setMap(arr,ids,coords);
+    var coords = {lat: 43.1, lng: -79.3};
+    setMap(lat,lon,ids,coords);
   }
 }
 
 
-function setMap(arr,ids,position){
+function setMap(lat,lon,ids,position){
 
     var mapDiv = document.getElementById("col1");
     map = new google.maps.Map(mapDiv, {
     center: position,
     zoom: 14
   });
+
+
   var geocoder = new google.maps.Geocoder();
-  for (var i = arr.length - 1; i >= 0; i--) {
-    getCoor(arr[i],map,geocoder,ids[i]);
-  };
-}
+  var num;
 
-
-function getCoor(address, map, geocoder, ids){
-
-  geocoder.geocode({'address':address}, function(results,status){
-    if (status === google.maps.GeocoderStatus.OK) {
-      var marker = new google.maps.Marker({
-      position: results[0].geometry.location,
+  for (var i = ids.length - 1; i >= 0; i--) {
+    var coor = new google.maps.LatLng(lat[i], lon[i]);
+    var marker = new google.maps.Marker({
+      position: coor,
       map: map,
-      title: address,
+      title: ids[i],
       icon: "../images/houseIcon.jpeg",
-    });
-      google.maps.event.addListener(marker, 'click', function(num) {
-        window.location.href = "houseTemplate/"+ids;
-      });
-  }
+    });  
+    
    
+    marker.addListener('click', function() {
+      window.location.href = "houseTemplate/" + this.title;
+    });
 
-    else {
-      //alert('Cannot compute Coordinates : ' + status);
-    };
+    marker.addListener('mouseover', function() {
+      this.setIcon(null);
+    });
+
+    marker.addListener('mouseout', function() {
+      this.setIcon("../images/houseIcon.jpeg");
+    });
+
+    
+
+
+  };
+  marker.addListener('click', function() {
+    map.setZoom(8);
+    map.setCenter(marker.getPosition());
   });
 }
 
-function success(position,arr,ids) {
-     var latitude = position.coords.latitude;
-     var longitude = position.coords.longitude;
+function success(position,lat,lon,ids) {
      coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-     setMap(arr,ids,coords);
+     setMap(lat,lon,ids,coords);
 }
