605a5ed8a23c5a7fa8a366ef040119e1b6551512
Alex Lawrence <Alex Lawrence>
2016-04-15 16:32:58 -0400

    Minor changes to layouts globally and on the signup/sign in pages.

--- resources/views/signIn.blade.php
+++ resources/views/signIn.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 
-
 <link href="{!! asset('CSS/globalStyles.css') !!}" media="all" rel="stylesheet" type="text/css" />
 <link href="{!! asset('CSS/signIn.css') !!}" media="all" rel="stylesheet" type="text/css" />
 <br>
@@ -47,10 +46,10 @@
 	<div class = "row">
 		<div class = "col-xs-1 col-sm-2 col-md-3"></div>
 		<div class = "col-xs-7 col-sm-5 col-md-4">
-			<button type="submit" class = "btn btn-default">Submit</button>
+			<button type="submit" class = "btn btn-default"> Sign In </button>
 		</div>
 		<div class = "col-xs-3 col-sm-3 col-md-2">
-			<a href="../passwordRetrieval" id = "forgotPass"> Forgot your password </a>
+			<a href="../passwordRetrieval" id = "forgotPass"> Forgot your password? </a>
 		</div>	
 	</div>
 
--- resources/views/signUp.blade.php
+++ resources/views/signUp.blade.php
@@ -7,7 +7,6 @@
 <link href="{!! asset('CSS/signUp.css') !!}" media="all" rel="stylesheet" type="text/css" />
 
 <div class = "pageTitle"> Sign Up </div>
-
 	@if (count($errors) > 0)
 	    <div class="alert alert-danger">
 	        <ul>
@@ -17,77 +16,70 @@
 	        </ul>
 	    </div>
 	@endif
-
 <form action = "signUp" method = "post">
 	<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-
 	<div class = "row">
-		<div class = "col-xs-1"></div>
-		<div class = "col-xs-5">
+		<div class = "col-xs-1 col-sm-2 col-md-3"></div>
+		<div class = "col-xs-5 col-sm-4 col-md-3">
 			First Name : 
 			<input type = "text" class = "form-control" name = "firstName" value="{{ old('firstName') }}">
 		</div>
-		<div class = "col-xs-5">
+		<div class = "col-xs-5 col-sm-4 col-md-3">
 			Last Name : 
 			<input type = "text" class = "form-control" name = "lastName" value="{{ old('lastName') }}">
 		</div>
-
 	</div>
 
 	<div class = "row">
-		<div class = "col-xs-1"></div>
-		<div class = "col-xs-6" id = "phoneNumDiv">
-				Phone Number : 
-				<div class = "controls form-inline">
-				<input type = "text" class = "form-control input-sm" placeholder="123-456-7890 x1234" name = "phone" maxlength = "18" value="{{ old('phone') }}">
-				</div>
+		<div class = "col-xs-1 col-sm-2 col-md-3"></div>
+		<div class = "col-xs-10 col-sm-8 col-md-6">
+			Phone Number : 
+			<!--<div class = "controls form-inline">-->
+			<input type = "text" class = "form-control" placeholder="123-456-7890 x1234" name = "phone" maxlength = "18" value="{{ old('phone') }}">
+			<!--</div>-->
 		<div class = "col-xs-5"></div>	
 		</div>
 	</div>
 
 	<div class = "row">
-		<div class = "col-xs-1"></div>
-		<div class = "col-xs-10">
+		<div class = "col-xs-1 col-sm-2 col-md-3"></div>
+		<div class = "col-xs-10 col-sm-4 col-md-3">
 			E-mail Address : 
 			<input type = "email" class = "form-control" name = "email" id = "username" value="{{ old('email') }}">
 		</div>
-	</div>
-
-	<div class = "row">
-		<div class = "col-xs-1"></div>
-		<div class = "col-xs-10">
+		<div class = "col-xs-12 col-sm-2 col-md-3"></div>
+		<div class = "col-xs-1 col-sm-2 col-md-3"></div>
+		<div class = "col-xs-10 col-sm-4 col-md-3">
 			Verify E-mail Address : 
 			<input type = "email" class = "form-control" name = "email_confirmation" value="{{ old('email') }}">
 		</div>
 	</div>
 
 	<div class = "row">
-		<div class = "col-xs-1"></div>
-		<div class = "col-xs-10">
+		<div class = "col-xs-1 col-sm-2 col-md-3"></div>
+		<div class = "col-xs-10 col-sm-4 col-md-3">
 			Password :  
 			<input type = "password" class = "form-control" name = "pass" id = "password">
 		</div>
-	</div>
-
-	<div class = "row">
-		<div class = "col-xs-1"></div>
-		<div class = "col-xs-10">
+		<div class = "col-xs-12 col-sm-2 col-md-3"></div>
+		<div class = "col-xs-1 col-sm-2 col-md-3"></div>
+		<div class = "col-xs-10 col-sm-4 col-md-3">
 			Verify Password :  
 			<input type = "password" class = "form-control" name = "pass_confirmation">
 		</div>
 	</div>
 
 	<div class = "row">
-		<div class = "col-xs-1"></div>
-		<div class = "col-xs-5">
+		<div class = "col-xs-1 col-sm-2 col-md-3"></div>
+		<div class = "col-xs-5 col-sm-4 col-md-3">
 			<button type="submit" class = "btn btn-default">Submit</button>
 		</div>
 	</div>
 
 	<div class = "row" id = "registerInquiry">
-		<div class = "col-xs-1"></div>
-		<div class = "col-xs-10">
+		<div class = "col-xs-3"></div>
+		<div class = "col-xs-6">
 			<div class = "panel panel-default">
 				Already Registered? <a href="../signIn"> Sign in </a>
 			</div>
