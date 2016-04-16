b41388aa2a5e2630b8d857c306234c4c5ef1735c
Andrew Rooney <adrt2k@cogeco.ca>
2016-04-16 14:45:54 -0400

    Revert "Revert "Added migration for jobs database""
    This reverts commit a62c8f46ddfaf77a93efea7a1a67eba3c29d96a6.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,24 +23,34 @@ class ComposerAutoloaderInitfd72ac248e6154256d1e5b915e4eec8a
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitfd72ac248e6154256d1e5b915e4eec8a', 'loadClassLoader'));
 
-        $map = require __DIR__ . '/autoload_namespaces.php';
-        foreach ($map as $namespace => $path) {
-            $loader->set($namespace, $path);
-        }
+        if (PHP_VERSION_ID >= 50600) {
+            require_once __DIR__ . '/autoload_static.php';
 
-        $map = require __DIR__ . '/autoload_psr4.php';
-        foreach ($map as $namespace => $path) {
-            $loader->setPsr4($namespace, $path);
-        }
+            call_user_func(\Composer\Autoload\ComposerStaticInitfd72ac248e6154256d1e5b915e4eec8a::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
 
-        $classMap = require __DIR__ . '/autoload_classmap.php';
-        if ($classMap) {
-            $loader->addClassMap($classMap);
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
         }
 
         $loader->register(true);
 
-        $includeFiles = require __DIR__ . '/autoload_files.php';
+        if (PHP_VERSION_ID >= 50600) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitfd72ac248e6154256d1e5b915e4eec8a::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequirefd72ac248e6154256d1e5b915e4eec8a($fileIdentifier, $file);
         }
